d5ddbe9483955fb2603b7951422e4b8a8d9eaa60
vickoritz <vickor@attensi.com>
2022-03-26 13:00:46 +0100

    Started new project, added html, css and js wip

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const emojiContainer = document.getElementById("emoji-container")
+const pushBtn = document.getElementById("push-btn")
+
+for (let i = 0; i < myEmojis.length; i++) {
+    const emoji = document.createElement('span')
+    emoji.textContent = myEmojis[i]
+    emojiContainer.append(emoji)
+}
+
+const pushBtn = document.getElementById("push-btn")
+pushBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
+    if (emojiInput.value) {
+       myEmojis.push(emojiInput.value)
+       emojiInput.value = ""
+       console.log(myEmojis)
+    }
+}) 
