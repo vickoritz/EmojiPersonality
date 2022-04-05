c092d82019f8049561bda8c9432356b0df440531
vickoritz <vickor@attensi.com>
2022-04-05 20:04:05 +0200

    add unshift btn to add emoji at the start of array

--- index.js
+++ index.js
@@ -3,20 +3,35 @@
 
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiContainer = document.getElementById("emoji-container")
-const pushBtn = document.getElementById("push-btn")
 
-for (let i = 0; i < myEmojis.length; i++) {
-    const emoji = document.createElement('span')
-    emoji.textContent = myEmojis[i]
-    emojiContainer.append(emoji)
+function renderEmojis() {
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
+    }
 }
+
+renderEmojis()
 
 const pushBtn = document.getElementById("push-btn")
 pushBtn.addEventListener("click", function(){
     const emojiInput = document.getElementById("emoji-input")
     if (emojiInput.value) {
-       myEmojis.push(emojiInput.value)
-       emojiInput.value = ""
-       console.log(myEmojis)
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        emojiContainer.innerHTML = ""
+        renderEmojis()
     }
-}) 
+})
+
+const unshiftBtn = document.getElementById("unshift-btn")
+unshiftBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = ""
+        emojiContainer.innerHTML = ""
+        renderEmojis()
+    }
+})
